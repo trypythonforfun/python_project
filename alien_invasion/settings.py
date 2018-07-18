1132e7177458d093cc3a4f231c3cc06875f5bd20
jack <23@qq.com>
2018-07-18 18:31:35 +0900

    add band

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,3 +8,6 @@ class Settings():
         self.screen_width = 600
         self.screen_height = 400
         self.bg_color = (230, 230, 230)
+        
+        # 飞船的设置
+        self.ship_speed_factor = 1.5
