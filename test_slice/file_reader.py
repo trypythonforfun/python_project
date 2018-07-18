9d1042319604bb912c370d3e959f5ad6fdba89fa
jack <23@qq.com>
2018-07-18 11:54:11 +0900

    first commit

---
+++ test_slice/file_reader.py
@@ -0,0 +1,16 @@
+filename = 'pi_digits.txt'
+with open(filename) as file_object:
+    #contents = file_object.read()
+    #print(contents.rstrip())
+    for line in file_object:
+        print(line.rstrip())
+        
+with open(filename) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+print(pi_string)
+print(len(pi_string))
---
+++ test_slice/hello_world.py
@@ -0,0 +1,20 @@
+name = "hello world"
+print(name.title())
+
+print("\tw\tw\tw\tw\tw\n\tw\tw\tw")
+print(" w w w w w w w w")
+
+my_foods = ["A","B","C"]
+friend_foods = my_foods
+he_foods = my_foods[:]
+print(my_foods)
+print(friend_foods)
+print(he_foods)
+my_foods.append("my")
+friend_foods.append("friend")
+he_foods.append("he")
+print(my_foods)
+print(friend_foods)
+print(he_foods)
+print("tab")
+print("4space")
