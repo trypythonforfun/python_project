7a036b78886b3dcc7141e341f8e5e237dcd97d60
jack <23@qq.com>
2018-07-19 16:23:34 +0900

    set mouse

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -40,7 +40,10 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
-    if button_clicked and not stats.game_active:    
+    if button_clicked and not stats.game_active:
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
+    
         # 重置游戏统计信息
         stats.reset_stats()
         stats.game_active = True
@@ -168,6 +171,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
     """检查是否有外星人到达了屏幕底端"""
