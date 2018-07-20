03275f96d3f4cc5566fcc5f7d2865690702a7a6a
jack <2321513393@qq.com>
2018-07-20 19:20:01 +0900

    save

--- test_slice/scatter_squares.py
+++ test_slice/scatter_squares.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
@@ -16,3 +17,4 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
