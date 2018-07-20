83dd8df59d7acf79bee8099421e12c64ff0a487f
jack <2321513393@qq.com>
2018-07-20 19:14:15 +0900

    auto color

--- test_slice/scatter_squares.py
+++ test_slice/scatter_squares.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
