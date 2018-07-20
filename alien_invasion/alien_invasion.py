ad92b25eaaf4067e3a3974813886b800ad46541f
jack <23@qq.com>
2018-07-20 15:12:41 +0900

    kk

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -15,7 +15,7 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+ 
     # 创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
 
