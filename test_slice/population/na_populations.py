5f4411303c1d1eab368534a39dd229c1bdf69dbe
jack <2321513393@qq.com>
2018-07-23 18:24:23 +0900

    add population

---
+++ test_slice/population/na_populations.py
@@ -0,0 +1,9 @@
+#~ import pygal
+#~ wm = pygal.Worldmap()
+
+import pygal.maps.world
+wm = pygal.maps.world.World()
+
+wm.title = 'Populations of Countries in North America'
+wm.add('North America', {'ca': 34126000, 'us': 309349000, 'mx': 113423000})
+wm.render_to_file('na_populations.svg')
