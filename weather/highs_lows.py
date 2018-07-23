e6b570ab4e9352ea1c2733dab5acd2aff64a6976
jack <2321513393@qq.com>
2018-07-23 16:19:36 +0900

    add low

--- weather/highs_lows.py
+++ weather/highs_lows.py
@@ -11,22 +11,27 @@ with open(filename) as f:
     #~ for index, column_header in enumerate(header_row):
         #~ print(index, column_header)
         
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         
         high = int(row[1])
         highs.append(high)
+        
+        low = int(row[3])
+        lows.append(low)
 
     #~ print(highs)
     
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # 设置图形的格式
-plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
