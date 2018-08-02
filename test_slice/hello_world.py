c136afa9a8c7a2120bad49815b47bb7c8672c10b
jack <2321513393@qq.com>
2018-08-02 16:19:43 +0900

    add guess number

--- test_slice/hello_world.py
+++ test_slice/hello_world.py
@@ -18,3 +18,18 @@ print(friend_foods)
 print(he_foods)
 print("tab")
 print("4space")
+
+def eggs(someParameter):
+    someParameter.append('Hello')
+    
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
+
+def egg():
+    global kk
+    kk = 2
+    
+kk = 3
+egg()
+print(kk)
