d63ed4a9559858b12167c965f919368ea5c6e1a7
jack <2321513393@qq.com>
2018-09-26 07:53:44 +0800

    add regex

--- web_api/url_soup/url_bs4.py
+++ web_api/url_soup/url_bs4.py
@@ -1,10 +1,12 @@
 #!/usr/local/bin/python3
 
-from urllib.request import urlopen
+from urllib.request import urlopen 
 from bs4 import BeautifulSoup 
+import re
 
 html = urlopen('https://en.wikipedia.org/wiki/Kevin_Bacon')
 bs = BeautifulSoup(html, 'html.parser')
-for link in bs.find_all('a'):
+for link in bs.find('div', {'id':'bodyContent'}).find_all(
+    'a', href=re.compile('^(/wiki/)((?!:).)*$')):
     if 'href' in link.attrs:
         print(link.attrs['href'])
