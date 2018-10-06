af076720eadba7b46e483e939540bd59d6f80d7b
jack <2321513393@qq.com>
2018-10-07 06:58:29 +0800

    add internallink externallink

--- web_api/url_soup/url_bs4.py
+++ web_api/url_soup/url_bs4.py
@@ -1,28 +1,57 @@
 #!/usr/local/bin/python3
 
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import re
+import datetime
+import random
 
 pages = set()
-def getLinks(pageUrl):
-    global pages
-    html = urlopen('https://en.wikipedia.org{}'.format(pageUrl))
+random.seed(datetime.datetime.now())
+
+#Retrieves a list of all Internal links found on a page
+def getInternalLinks(bs, includeUrl):
+    includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme, urlparse(includeUrl).netloc)
+    internalLinks = []
+    #Finds all links that begin with a "/"
+    for link in bs.find_all('a', href=re.compile('^(/|.*'+includeUrl+')')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                if(link.attrs['href'].startswith('/')):
+                    internalLinks.append(includeUrl+link.attrs['href'])
+                else:
+                    internalLinks.append(link.attrs['href'])
+    return internalLinks
+            
+#Retrieves a list of all external links found on a page
+def getExternalLinks(bs, excludeUrl):
+    externalLinks = []
+    #Finds all links that start with "http" that do
+    #not contain the current URL
+    for link in bs.find_all('a', href=re.compile('^(http|www)((?!'+excludeUrl+').)*$')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+ 
+def getRandomExternalLink(startingPage):
+    html = urlopen(startingPage)
     bs = BeautifulSoup(html, 'html.parser')
-    try:
-        print(bs.h1.get_text())
-        print(bs.find(id ='mw-content-text').find_all('p')[0])
-        print(bs.find(id='ca-edit').find('span').find('a').attrs['href'])
-    except AttributeError:
-        print('This page is missing something! Continuing.')
+    externalLinks = getExternalLinks(bs, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        print('No external links, looking around the site for one')
+        domain = '{}://{}'.format(urlparse(startingPage).scheme, urlparse(startingPage).netloc)
+        internalLinks = getInternalLinks(bs, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0,
+                                    len(internalLinks)-1)])
+    else:
+        return externalLinks[random.randint(0, len(externalLinks)-1)]
     
-    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
-        if 'href' in link.attrs:
-            if link.attrs['href'] not in pages:
-                #We have encountered a new page
-                newPage = link.attrs['href']
-                print('-'*20)
-                print(newPage)
-                pages.add(newPage)
-                getLinks(newPage)
-getLinks('')
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print('Random external link is: {}'.format(externalLink))
+    followExternalOnly(externalLink)
+            
+followExternalOnly('http://www.163.com')
